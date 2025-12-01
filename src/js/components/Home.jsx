820586cace1c32838e43adb5c57640a076033fe9
Azalea <carla.clemente.010@gmail.com>
2025-12-01 12:18:58 +0100

    fixed issues regarding deleting and id management

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,42 @@
 import React, {useState} from "react";
 
 function Home() {
-
-  const [toDos, setToDos] = useState(["laundry", "coding"]);
+  const [toDos, setToDos] = useState([{task:"laundry", id: 0},{task:"coding", id:1}]);
   const [inputValue, setInputValue] = useState('')
+  let counter = 1;
 
-      const handleFormSubmit = (e) => {
-          e.preventDefault();
-          setToDos([...toDos, inputValue]);
-      }
+  function generateId() {
+    counter +=1;
+    return counter;
+  }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        if(!inputValue.trim()){
+          return;
+        }
+        setToDos([...toDos, {task: inputValue, id: generateId}]);
+        setInputValue("");
+    }
   
-      const deleteToDo = (toDo) => {
-          setToDos(toDos.filter(str => str !== toDo));
-      }
+    const deleteToDo = (id) => {
+      setToDos(toDos.filter(i => i.id !== id));
+    }
       
-      const renderToDos = toDos.map((toDo, index)=> (
-          <li key={index}>
-              <div className="position-relative todo">
-                  <label>{toDo}</label>
-                  <button className="delete position-absolute end-0 top-50 translate-middle" onClick={() => deleteToDo(toDo)}>x</button>
-              </div>
-          </li>
-      ));
+    const renderToDos = (toDos.length !== 0 ? 
+      (toDos.map((toDo, index) => (
+        <li key={index}>
+            <div className="position-relative todo">
+                <label>{toDo.task}</label>
+                <button className="delete position-absolute end-0 top-50 translate-middle" onClick={() => deleteToDo(toDo.id)}>x</button>
+            </div>
+        </li>))): 
+        (<li key="0">
+            <div className="position-relative todo">
+                <label>There are no tasks</label>
+            </div>
+        </li>)
+    );
 
   return (
     <div className="App">
@@ -39,8 +54,10 @@ function Home() {
                   onChange={(e) => setInputValue(e.target.value)} />
           </form>
           </div>
-          <div>
-            {renderToDos}
+          <div className="list">
+            <ul>
+              {renderToDos}
+            </ul>
         </div>
       </div>
     </div>
